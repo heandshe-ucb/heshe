3226611d116b44c23a5555842a2b71347ceaef90
Navin K Jammula <navin_kumarj@intuit.com>
2018-03-03 10:30:38 -0800

    added schema and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,24 @@ CREATE DATABASE heandshe_db;
 
 USE heandshe_db;
 
--- CREATE TABLE burgers (
---     id INT NOT NULL AUTO_INCREMENT, 
---     burger_name VARCHAR(255) NULL,
---     devoured BOOLEAN DEFAULT false,
---     PRIMARY KEY (id)
--- );
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username NOT NULL VARCHAR(255), 
+    userpassword VARCHAR(255) NULL,
+    firstname VARCHAR(255) NULL,
+    lastname VARCHAR(255) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE usersearches (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    searchlocation VARCHAR(255) NOT NULL,
+    traveldistance INT(10) NOT NULL,
+    hourstospend INT(10) NOT NULL,
+    userdate VARCHAR(255) NOT NULL,
+    starttime VARCHAR(4) NOT NULL,
+    experiencetype VARCHAR(1020) NOT NULL,
+    searchtime DATETIME DEFAULT   CURRENT_TIMESTAMP
+    PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,17 @@
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Chicken Burger", false);
+INSERT INTO users (username, userpassword, firstname, lastname)
+VALUES ("navinjoy@gmail.com", "heshe1", "Navin", "Kumar");
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Cheese Burger", false);
+INSERT INTO users (username, userpassword, firstname, lastname)
+VALUES ("stephendklein08@gmail.com", "heshe2", "Stephen", "Klein");
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Fish Burger", false);
+INSERT INTO users (username, userpassword, firstname, lastname)
+VALUES ("mrswinlee@gmail.com", "heshe3", "Win", "Lee");
+
+INSERT INTO users (username, userpassword, firstname, lastname)
+VALUES ("vnavarro138@gmail.com", "heshe4", "Veronica", "Navarro");
+
+
+-- Below sample data for usersearches
+
+INSERT INTO usersearches (username, searchlocation, traveldistance, hourstospend, userdate, starttime, experiencetype)
+VALUES ("navinjoy@gmail.com", "Fremont, CA", "5", "3", "3/3/2018", "2100", "['Romantic','Outdoor','Nature']");
