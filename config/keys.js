6c0a4c6a174b28876ca2c26f373cf10ed902520c
Navin K Jammula <navin_kumarj@intuit.com>
2018-03-08 21:08:55 -0800

    updated google api keys

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,10 @@ module.exports = {
     google: {
         // clientID: "706823790963-0umq1a2pcj17j0pikb5vkflpm1slrkuo.apps.googleusercontent.com",
         // clientSecret: "eZfHyydhUR0q8Mk8ys0cGLSg"
-        clientID: "170295732516-6e99de0hdj6m5je31uisnkn7fv2poprs.apps.googleusercontent.com",
-        clientSecret: "b0qpDHbOQ7Bl9qvFq8sG8yNE"
+        // clientID: "170295732516-6e99de0hdj6m5je31uisnkn7fv2poprs.apps.googleusercontent.com",
+        // clientSecret: "b0qpDHbOQ7Bl9qvFq8sG8yNE"
+
+        clientID:"170295732516-uuau9mbnkotbkgufantk5igd2b9fuf3j.apps.googleusercontent.com",
+        clientSecret: "uML-S9s0nl12vi9jM0aq2AV8"
     }
 };
